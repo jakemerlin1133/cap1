6a600db13d07f3c171a2437f64cdda36c5240efd
jakemerlin1133 <jakemerlin123@gmail.com>
2025-06-11 10:58:39 +0800

    icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <title>@yield('title', 'Kabaleyan Cove Resort')</title>
     <link rel="icon" type="image/png" href="{{ asset('KCR.png') }}">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="https://kit.fontawesome.com/5ccb4e6d08.js" crossorigin="anonymous"></script>
     <link href="/src/styles.css" rel="stylesheet">
 </head>
 <body class="flex bg-[#FFFDF6]">
@@ -14,11 +15,13 @@
    @include('partials.sidebar')
 
 <div class="flex flex-col flex-1 min-h-screen">
+
         {{-- Header --}}
-        <header class="bg-[#A0C878] text-white p-3 shadow flex">
-            <img src="{{ asset('KCR.png') }}" alt="" width="25" height="25" class="inline-block mr-2">
-            <h1 class="text-md font-semibold italic text-[#FAF6E9]">Kabaleyan Cove Resort</h1>
-        </header>
+    <header class="bg-[#A0C878] text-white p-3 shadow flex items-center gap-2">
+        <i class="fa-solid fa-bars text-xl mr-2 text-[#FAF6E9]"></i>
+        <img src="{{ asset('KCR.png') }}" alt="Logo" class="w-6 h-6 object-contain">
+        <h1 class="text-md font-semibold italic text-[#FAF6E9]">Kabaleyan Cove Resort</h1>
+    </header>
 
         {{-- Main Content --}}
         <main class="flex-1 p-6">
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,11 +1,43 @@
-<aside class="w-64 bg-[#73946B] text-white min-h-screen p-4">
-    <h1 class="text-3xl text-[#FAF6E9] font-bold my-4 text-center">Counter 1</h1>
+<aside class="w-64 bg-[#73946B] text-white min-h-screen">
+    <h1 class="text-3xl text-[#FAF6E9] bg-[#73946B] font-bold my-4 text-center">Counter 1</h1>
     <ul>
-        <li><a href="{{ route('dashboard') }}" class="block py-2 text-[#FAF6E9] font-semibold">Dashboard</a></li>
-        <li><a href="{{ route('order') }}" class="block py-2 text-[#FAF6E9] font-semibold">Order</a></li>
-        <li><a href="{{ route('inventory') }}" class="block py-2 text-[#FAF6E9] font-semibold">Inventory</a></li>
-        <li><a href="{{ route('accountList') }}" class="block py-2 text-[#FAF6E9] font-semibold">Account List</a></li>
-        <li><a href="{{ route('purchaseHistory') }}" class="block py-2 text-[#FAF6E9] font-semibold">Purchase History</a></li>
-        <li><a href="{{ route('profile') }}" class="block py-2 text-[#FAF6E9] font-semibold">Profile</a></li>
+
+        <li class="flex items-center gap-2 pl-6 py-1 {{ request()->routeIs('dashboard') ? 'bg-[#537D5D]' : '' }}">
+            <i class="fa-solid fa-house"></i>
+            <a href="{{ route('dashboard') }}"
+                class="block py-2 text-[#FAF6E9] font-semibold">Dashboard</a>
+        </li>
+
+
+         <li class="flex items-center gap-2 pl-6 py-1 {{ request()->routeIs('order') ? 'bg-[#537D5D]' : '' }}">
+            <i class="fa-solid fa-cart-shopping"></i>
+            <a href="{{ route('order') }}"
+                class="block py-2 text-[#FAF6E9] font-semibold">Order</a>
+        </li>
+
+         <li class="flex items-center gap-2 pl-6 py-1 {{ request()->routeIs('inventory') ? 'bg-[#537D5D]' : '' }}">
+            <i class="fa-solid fa-warehouse"></i>
+            <a href="{{ route('inventory') }}" 
+                class="block py-2 text-[#FAF6E9] font-semibold">Inventory</a>
+        </li>
+
+         <li class="flex items-center gap-2 pl-6 py-1 {{ request()->routeIs('accountList') ? 'bg-[#537D5D]' : '' }}">
+            <i class="fa-solid fa-circle-user"></i>
+            <a href="{{ route('accountList') }}" 
+                class="block py-2 text-[#FAF6E9] font-semibold">Account List</a>
+        </li>
+
+        <li class="flex items-center gap-2 pl-6 py-1 {{ request()->routeIs('purchaseHistory') ? 'bg-[#537D5D]' : '' }}">
+            <i class="fa-solid fa-shop"></i>
+            <a href="{{ route('purchaseHistory') }}" 
+                class="block py-2 text-[#FAF6E9] font-semibold">Purchase History</a>
+        </li>
+
+         <li class="flex items-center gap-2 pl-6 py-1 {{ request()->routeIs('profile') ? 'bg-[#537D5D]' : '' }}">
+            <i class="fa-solid fa-user"></i>
+            <a href="{{ route('profile') }}" 
+                class="block py-2 text-[#FAF6E9] font-semibold">Profile</a>
+        </li>
+
     </ul>
 </aside>
